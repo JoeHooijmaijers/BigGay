4f2424f7e2db3a64062fb063866b481c2dec13d1
JoeHooijmaijers <31613736+JoeHooijmaijers@users.noreply.github.com>
2018-04-09 16:49:28 +0200

    Initial upload

---
+++ RPGKillerApp/RPGKillerApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RPGKillerApp.MvcApplication" Language="C#" %>
---
+++ RPGKillerApp/RPGKillerApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RPGKillerApp.Startup))]
+namespace RPGKillerApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
